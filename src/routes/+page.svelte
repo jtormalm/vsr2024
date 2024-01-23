8c735d330c53e403a337c2e967beb6893f8e6cff
Jakob Tormalm <jakob.tormalm@gmail.com>
2024-01-23 23:10:27 +0100

    Maybe final?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 
 	import { getModalStore } from '@skeletonlabs/skeleton';
 	import { cubicInOut } from 'svelte/easing';
+	import { Copyright } from 'lucide-svelte';
 
 	const modalStore = getModalStore();
 
@@ -16,6 +17,8 @@
 	let innerWidth: number;
 	let innerHeight: number;
 	let loaded = false;
+
+	let targetDate = new Date('2024-02-01T10:00:00');
 
 	const textSizeBase = 180;
 	let initialScale = 1;
@@ -34,6 +37,15 @@
 			clockOpacity = clockWidth / (innerWidth * 0.5);
 		}
 	}
+
+	const calculateDaysLeft = (targetDate: Date): number => {
+		const now = new Date();
+		const difference = targetDate.getTime() - now.getTime();
+		const days = Math.ceil(difference / (1000 * 3600 * 24));
+		return days;
+	};
+
+	let daysLeft = calculateDaysLeft(targetDate);
 
 	const openTicketModal = () => {
 		const modal: ModalSettings = {
@@ -68,14 +80,16 @@
 
 <SaucerBackground />
 
-{#if scrollY < 1500}
-	<div
-		transition:fade={{ easing: cubicInOut, duration: 150 }}
-		class="fixed font-medium text-sm sm:text-base bottom-4 w-full text-center"
-	>
-		Skrolla ner
-	</div>
-{/if}
+<div
+	class="fixed flex flex-col items-center font-medium text-sm sm:text-base bottom-4 w-full text-center"
+>
+	{#if scrollY < 1500}
+		<span transition:fade={{ easing: cubicInOut, duration: 150 }}> Skrolla ner </span>
+	{/if}
+	<span class="flex gap-1 items-center">
+		<Copyright class="w-4" /> Jakob Tormalm - Simon Bonnedahl
+	</span>
+</div>
 
 <div class="fixed w-full items-center gap-4 flex top-4 left-4">
 	{Math.min(Math.max(Math.round((scrollY * 100) / 550), 0), 1000)}
@@ -114,7 +128,7 @@
 			>
 				<div style="opacity: {Math.min(0.5, clockOpacity)};" class="-mb-2">1-3.2.2024</div>
 				<div style="opacity: {clockOpacity};">
-					<Countdown targetDate={new Date('2024-02-01T10:00:00')} precision={5} />
+					<Countdown {targetDate} precision={5} />
 				</div>
 			</div>
 		</div>
@@ -123,17 +137,17 @@
 
 <div class="h-[5400px] flex justify-center"></div>
 
-<div class="flex flex-col gap-8 items-center w-full justify-center sticky top-12 sm:top-16">
+<div class="flex flex-col gap-6 items-center w-full justify-center sticky top-12 sm:top-16">
 	<div class="grid grid-cols-3 place-items-center items-center gap-4 sm:gap-8 px-2 sm:w-1/2">
-		<img class="h-full" src="/images/nira.svg" alt="Ericsson" />
-		<img class="h-full" src="/images/korallen.svg" alt="Ericsson" />
-		<img class="h-full" src="/images/coknajs.svg" alt="Microtec" />
+		<img src="/images/nira.svg" alt="Ericsson" />
+		<img src="/images/korallen.svg" alt="Ericsson" />
+		<img src="/images/coknajs.svg" alt="Microtec" />
 	</div>
 	<div class="px-2 sm:w-1/2 text-center font-medium text-2xl sm:text-3xl">
 		Norra Europas Största <br /> Parallel Saucer Race <br />
-		4 dagar kvar
+		<span class="font-bold"> {daysLeft} dagar kvar </span>
 	</div>
-	<div class="grid grid-cols-2 gap-4 w-full px-2 sm:px-0 font-medium sm:w-1/2">
+	<div class="grid grid-cols-2 gap-4 w-full px-2 sm:px-0 font-bold sm:w-1/2">
 		<button on:click={openTicketModal} class="btn w-full variant-filled-primary" type="button"
 			>Biljettsläpp</button
 		>
@@ -152,11 +166,17 @@
 			type="button">LÖRDAG</button
 		>
 	</div>
-	<div class="flex justify-center w-40 mt-16">
+	<div class="w-full px-2 sm:px-0 sm:w-1/2">
+		<button
+			class="btn variant-glass-primary text-center w-full text-2xl sm:text-3xl font-bold"
+			disabled>Öppna Tävling</button
+		>
+	</div>
+	<div class="flex justify-center w-40 sm:w-48 2xl:w-64">
 		<img class="" src="/images/y6.png" alt="YSex Logo" />
 	</div>
 </div>
-<div class="h-[300px] sm:h-[400px] flex justify-center"></div>
+<div class="h-[500px] sm:h-[1000px] flex justify-center"></div>
 
 <style>
 	.translateNone {
